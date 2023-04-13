46b771d4ba0792d3c21e17cbfabfb9552eb53726
FranciscoEchenique <echeniquefrancisco127@gmail.com>
2023-04-13 18:10:35 -0300

    Arreglando errores de produccion de nuevo

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors());
 
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/api/users', usersRoute);
