25eb9042ee37b53354a7d9b979e9920e1726f37a
FranciscoEchenique <echeniquefrancisco127@gmail.com>
2023-04-13 22:59:08 -0300

    Quitando log de produccion

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -35,8 +35,6 @@ const usersController = {
         try {
         const [rows] = await pool.query('INSERT INTO users(rol_id, name, lastname, email, password, image) VALUES (?, ?, ?, ?, ?, ?)', [rol_id, name, lastname, email, password, image] )
 
-        console.log(rows);
-
         res.send({ 
             id: rows.insertId,
             rol_id,
